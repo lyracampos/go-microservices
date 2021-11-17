dea49d041c750e8f7e4b6296cba9140807c55a30
Guilherme Lyra <lyracampos@gmail.com>
2021-11-17 18:58:43 -0300

    creating get, post e put for product-api.

---
+++ product-api/handlers/products.go
@@ -0,0 +1,103 @@
+package handlers
+
+import (
+	"go-microservices/product-api/data"
+	"log"
+	"net/http"
+	"regexp"
+	"strconv"
+)
+
+type Products struct {
+	log *log.Logger
+}
+
+func NewProducts(log *log.Logger) *Products {
+	return &Products{log}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		p.log.Println("PUT", r.URL.Path)
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			p.log.Println("Invalid URI more than one id")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			p.log.Println("Invalid URI more than one capture group")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			p.log.Println("Invalid URI unable to convert to numer", idString)
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		p.updateProduct(id, rw, r)
+		return
+	}
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.log.Println("handler GET products")
+
+	product := data.GetProducts()
+
+	err := product.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "unable to marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.log.Println("Handle POST product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	data.AddProduct(prod)
+}
+
+func (p *Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
+	p.log.Println("Handle PUT product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "product not found", http.StatusInternalServerError)
+		return
+	}
+}
